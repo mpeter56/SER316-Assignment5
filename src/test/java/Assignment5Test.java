bdabc77e7c2f103f83ae1c5b6dd95ddaef2f0bb5
Maura Peterson <mpeter56@asu.edu>
2022-11-13 10:39:30 -0700

    Initial setup

---
+++ src/test/java/Assignment5Test.java
@@ -0,0 +1,15 @@
+package test.java;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Assignment5Test {
+    
+    @Test
+    public void test() {
+        assertEquals(1,1);
+    }
+}
