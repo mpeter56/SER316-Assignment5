c25599fa826a4a5f9ffa3ee9a821afc81fcdee0f
Maura Peterson <mpeter56@asu.edu>
2022-11-13 11:43:40 -0700

    Fix setup so gradle will run

--- src/test/java/Assignment5Test.java
+++ src/test/java/Assignment5Test.java
@@ -1,10 +1,12 @@
 package test.java;
 
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+
 
 public class Assignment5Test {
     
